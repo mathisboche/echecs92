da739f760fc52f39af209ae3771674c35db607d2
Mathis Boche <mathisboche@gmail.com>
2025-10-25 14:56:56 +0200

    plugin

---
+++ wp-content/plugins/echecs92-core/echecs92-core.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Echecs92 Core
+ * Description: Fonctions cœur du Comité des Échecs 92 (clubs, événements, etc.).
+ * Author: Comité des Échecs 92
+ * Version: 0.1
+ */
+
+if (!defined('ABSPATH')) exit;
