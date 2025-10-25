5cba1ae35361ced78b489e0408c25443f80d77ef
Mathis Boche <mathisboche@gmail.com>
2025-10-25 18:54:14 +0200

    fix functions.php enqueue syntax (recover prod)

--- wp-content/themes/echecs92-child/functions.php
+++ wp-content/themes/echecs92-child/functions.php
@@ -1,10 +1,17 @@
 <?php
+if (!defined('ABSPATH')) exit;
+
+// Charger le CSS et le JS du thème enfant
 add_action('wp_enqueue_scripts', function () {
+    // CSS du thème enfant
     wp_enqueue_style(
         'echecs92-child',
         get_stylesheet_uri(),
         [],
         '1.0'
+    );
+
+    // JS du header (menu burger)
     wp_enqueue_script(
         'echecs92-header',
         get_stylesheet_directory_uri() . '/header.js',
@@ -12,5 +19,4 @@ add_action('wp_enqueue_scripts', function () {
         '1.0',
         true
     );
-
 });
