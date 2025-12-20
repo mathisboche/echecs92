3ef3bb7c28a5e4e9fdc7138ba37930b81fcb433a
Mathis Boche <mathisboche@gmail.com>
2025-12-20 15:45:43 +0100

    h

--- wp-content/themes/echecs92-child/header.js
+++ wp-content/themes/echecs92-child/header.js
@@ -402,8 +402,8 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     });
 
-    // Si on passe en desktop (>900px), on force fermé
-    window.matchMedia('(min-width: 900px)').addEventListener('change', (e) => {
+    // Si on passe en desktop (>960px), on force fermé
+    window.matchMedia('(min-width: 960px)').addEventListener('change', (e) => {
       if (e.matches) {
         closeMenu();
       }
