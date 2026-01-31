d4b21b7dc4a9c25a5327317f05ac3deba9bf4575
Mathis Boche <mathisboche@gmail.com>
2026-01-31 21:42:12 +0100

    chore: remove recaptcha secrets from repo

---
+++ wp-content/themes/echecs92-child/config/recaptcha.example.php
@@ -0,0 +1,10 @@
+<?php
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+return [
+    // Copy this file to recaptcha.php and fill in your own keys.
+    'site_key' => 'YOUR_SITE_KEY',
+    'secret_key' => 'YOUR_SECRET_KEY',
+];
