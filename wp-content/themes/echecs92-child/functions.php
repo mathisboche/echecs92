56dd46b7b393d1817eadb3a10691f1bfb2daeef0
Mathis Boche <mathisboche@gmail.com>
2026-02-21 15:41:59 +0100

    Uniformiser espacement cards actu

--- wp-content/themes/echecs92-child/functions.php
+++ wp-content/themes/echecs92-child/functions.php
@@ -682,7 +682,7 @@ function cdje92_output_runtime_easter_egg_config() {
         'trigger' => CDJE92_IG_CINEMA_TRIGGER,
         'alias'   => CDJE92_IG_CINEMA_ALIAS,
         'href'    => '',
-        'text'    => 'lien secret',
+        'text'    => 'mathisboche',
         'issueUrl' => esc_url_raw( rest_url( 'cdje92/v1/mathis-egg-url' ) ),
         'consumeUrl' => '',
     ];
